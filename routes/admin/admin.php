2d83e7a1b0c66929e7150b6be454875f2476296f
lvkt0177 <lvkt0177@gmail.com>
2026-08-09 01:26:32 +0700

    Alphakids: Branch - Ver 2 + Cau hinh

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -3,14 +3,10 @@
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth.admin')->group(function () {
+// Route::middleware('auth.admin')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     include 'branch.php';
-    include 'activity.php';
-    include 'faq.php';
-    include 'registration.php';
-    include 'setting.php';
 
-});
+// });
