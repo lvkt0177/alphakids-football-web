76d026217ec40953d7ea49cb9459c44fa8ec4be2
lvkt0177 <lvkt0177@gmail.com>
2026-09-04 10:10:58 +0700

    Alphakids: Update Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,14 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Admin',
             'username' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('Admin_3988'),
         ]);
 
-        User::factory()->create([
+        User::create([
             'name' => 'Nguyễn Công Lập',
             'username' => 'lapnc',
             'email' => 'lapnc@gmail.com',
